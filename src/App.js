e82411b2f6ce4d83a24f576d57246d7a78d0261c
Dilesh Bisen <2dileshbisen@gmail.com>
2025-02-06 22:07:14 +0530

    create new files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import './App.css';
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Header } from './_components';
+import { Header, HomePage, Exchanges, Cryptocurrencies, Cryptodetails, News } from './_components';
 
 function App() {
   return (
     <div className='app'>
       <div className='header'>
-        <Header/>
+        <Header />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <HomePage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies '>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <Cryptodetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
